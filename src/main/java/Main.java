383dc2cfdbaf07886ec5d68a3dfe299fb1eda225
Charlie <ruggedcafe@gmail.com>
2021-11-30 01:09:31 +0900

    feat: createRandomNumber 메소드 구현
    second_try.baseball 패키지의 NumberGenerator 클래스 사용

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import first_try.baseball.Player;
 import second_try.baseball.domain.Calculator;
+import second_try.baseball.domain.NumberGenerator;
 
 public class Main {
     public static void main(String[] args) {
         /*Player player = new Player();
         player.gameStart();*/
-        Calculator calculator1 = new Calculator();
+        /*Calculator calculator1 = new Calculator();
         calculator1.add(2, 2);
         System.out.println(Calculator.result);
 
@@ -13,6 +14,10 @@ public class Main {
         calculator2.add(3, 3);
         System.out.println(Calculator.result);
         System.out.println(Calculator.result);
-        System.out.println(Calculator.SHARE_BIRTHDAY);
+        System.out.println(Calculator.SHARE_BIRTHDAY);*/
+
+        NumberGenerator numberGenerator = new NumberGenerator();
+
+        System.out.println(numberGenerator.createRandomNumber());
     }
 }
