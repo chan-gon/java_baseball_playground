dda3eaecdf087a12fa89ad4b318e69abc9b0ce13
Charlie <ruggedcafe@gmail.com>
2021-11-29 18:06:18 +0900

    refactor: BallTest 클래스 수정
    PlayResult 클래스 추가

--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Balls {
-    private final List<Ball> balls;
+    private final List<Ball> answers;
 
     public Balls(List<Integer> answers) {
-        this.balls = getBalls(answers);
+        this.answers = getBalls(answers);
     }
 
     private static List<Ball> getBalls(List<Integer> answers) {
@@ -18,8 +18,18 @@ public class Balls {
         return balls;
     }
 
+    public PlayResult play(List<Integer> balls) {
+        Balls userBalls = new Balls(balls);
+        PlayResult result = new PlayResult();
+        for (Ball answer : answers) {
+            BallStatus status = userBalls.play(answer);
+            result.report(status);
+        }
+        return result;
+    }
+
     public BallStatus play(Ball userBall) {
-        return balls.stream()
+        return answers.stream()
                 .map(answer -> answer.play(userBall))
                 .filter(BallStatus::isNotNothing) // Enum도 하나의 클래스이고 객체이기 때문에 메시지를 통해 접근한다
                 .findFirst()
--- src/test/java/baseball/BallsTest.java
+++ src/test/java/baseball/BallsTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,6 +9,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallsTest {
 
+    @DisplayName("세 개의 값과 세 개의 응답")
+    @Test
+    void play() {
+        Balls answers = new Balls(Arrays.asList(1, 2, 3));
+        PlayResult result = answers.play(Arrays.asList(4, 5, 6));
+        assertThat(result.getStrike()).isEqualTo(0);
+        assertThat(result.getBall()).isEqualTo(0);
+    }
+
+    @DisplayName("세 개의 값과 세 개의 응답")
+    @Test
+    void play_1strike_1ball() {
+        Balls answers = new Balls(Arrays.asList(1, 2, 3));
+        PlayResult result = answers.play(Arrays.asList(1, 4, 2));
+        assertThat(result.getStrike()).isEqualTo(1);
+        assertThat(result.getBall()).isEqualTo(1);
+    }
+
+    @DisplayName("세 개의 값과 세 개의 응답")
+    @Test
+    void play_3strike() {
+        Balls answers = new Balls(Arrays.asList(1, 2, 3));
+        PlayResult result = answers.play(Arrays.asList(1, 2, 3));
+        assertThat(result.getStrike()).isEqualTo(3);
+        assertThat(result.isGameEnd()).isTrue();
+    }
+
+    @DisplayName("세 개의 값과 하나의 응답_strike")
     @Test
     void strike() {
         Balls answers = new Balls(Arrays.asList(1, 2, 3));
@@ -15,6 +44,7 @@ public class BallsTest {
         assertThat(status).isEqualTo(BallStatus.STRIKE);
     }
 
+    @DisplayName("세 개의 값과 하나의 응답_ball")
     @Test
     void ball() {
         Balls answers = new Balls(Arrays.asList(1, 2, 3));
@@ -22,6 +52,7 @@ public class BallsTest {
         assertThat(status).isEqualTo(BallStatus.BALL);
     }
 
+    @DisplayName("세 개의 값과 하나의 응답_nothing")
     @Test
     void nothing() {
         Balls answers = new Balls(Arrays.asList(1, 2, 3));
