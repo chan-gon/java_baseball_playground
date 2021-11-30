a53c4127e85bfdb162945e4e57efb1e160f2e4bc
Charlie <ruggedcafe@gmail.com>
2021-11-30 12:21:59 +0900

    feat: 컴퓨터와 플레이어 숫자 비교 기능 구현
    Referee/compare

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,14 @@ import first_try.baseball.Player;
 import second_try.baseball.domain.Calculator;
 import second_try.baseball.domain.Judgement;
 import second_try.baseball.domain.NumberGenerator;
+import second_try.baseball.domain.Referee;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        /*Player player = new Player();
-        player.gameStart();*/
-        /*Calculator calculator1 = new Calculator();
-        calculator1.add(2, 2);
-        System.out.println(Calculator.result);
+        Referee referee = new Referee();
 
-        Calculator calculator2 = new Calculator();
-        calculator2.add(3, 3);
-        System.out.println(Calculator.result);
-        System.out.println(Calculator.result);
-        System.out.println(Calculator.SHARE_BIRTHDAY);*/
-
-        Judgement judgement = new Judgement();
-        boolean result = judgement.hasPlace(Arrays.asList(1, 2, 3), 1, 2);
-        System.out.println(result);
+        System.out.println(referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(3, 4, 5)));
     }
 }
--- src/main/java/second_try/baseball/domain/Referee.java
+++ src/main/java/second_try/baseball/domain/Referee.java
@@ -4,6 +4,17 @@ import java.util.List;
 
 public class Referee {
     public String compare(List<Integer> computer, List<Integer> player) {
-        return "strike";
+
+        Judgement judgement = new Judgement();
+        int cnt = judgement.correctCount(computer, player);
+
+        int strike = 0;
+        for (int i = 0; i < player.size(); i++) {
+            if (judgement.hasPlace(computer, i, player.get(i))) {
+                strike++;
+            }
+        }
+        int ball = cnt - strike;
+        return ball + "볼 " + strike + "스트라이크";
     }
 }
