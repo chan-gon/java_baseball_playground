509b50fe841343b7f730b0bb23129f6b05c31837
Charlie <ruggedcafe@gmail.com>
2021-12-01 23:54:37 +0900

    refactor: checkNumber 메소드 결과 출력 로직 수정
    showResult() 메소드 추가

---
+++ src/main/java/third_try/baseball/JudgeNumber.java
@@ -0,0 +1,20 @@
+package third_try.baseball;
+
+import java.util.List;
+
+public class JudgeNumber {
+
+    public int correctCount(List<Integer> computer, List<Integer> player) {
+        int result = 0;
+        for (int i = 0; i < player.size(); i++) {
+            if (computer.contains(player.get(i))) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public boolean hasPlace(List<Integer> computer, int placeIndex, int number) {
+        return computer.get(placeIndex) == number;
+    }
+}
--- src/main/java/third_try/baseball/PlayGame.java
+++ src/main/java/third_try/baseball/PlayGame.java
@@ -5,42 +5,39 @@ import java.util.List;
 public class PlayGame {
     Computer computer = new Computer();
     Player player = new Player();
+    JudgeNumber judgeNumber = new JudgeNumber();
 
     public String checkNumber(List<Integer> computerNumber, List<Integer> playerNumber) {
 
 /*        computerNumber = computer.generateNumbers();
         playerNumber = player.createInputNumbers();*/
-        int cnt = correctCount(computerNumber, playerNumber);
+        int cnt = judgeNumber.correctCount(computerNumber, playerNumber);
 
         int strike = 0;
         int ball = 0;
 
         for (int i = 0; i < 3; i++) {
-            if (hasPlace(computerNumber, i, playerNumber.get(i))) {
+            if (judgeNumber.hasPlace(computerNumber, i, playerNumber.get(i))) {
                 strike++;
             }
-
             ball = cnt - strike;
-
-            if (cnt == 0) {
-                return "낫싱";
-            }
         }
-        return ball + "볼 " + strike + "스트라이크";
-    }
+        String result = showResult(ball, strike, cnt);
 
-    public int correctCount(List<Integer> computer, List<Integer> player) {
-        int result = 0;
-        for (int i = 0; i < player.size(); i++) {
-            if (computer.contains(player.get(i))) {
-                result++;
-            }
-        }
         return result;
     }
 
-    public boolean hasPlace(List<Integer> computer, int placeIndex, int number) {
-        return computer.get(placeIndex) == number;
+    public String showResult(int ball, int strike, int cnt) {
+        String result = "";
+        if (ball > 0) {
+            result = ball + "볼";
+        }
+        if (strike > 0) {
+            result = strike + "스트라이크";
+        }
+        if (cnt == 0) {
+            result = "낫싱";
+        }
+        return result;
     }
-
 }
